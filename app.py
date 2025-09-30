cff36d24be092c19b9f06e3bc72815073ea9f340
AbdullahAlFalah <salem908mk@gmail.com>
2025-09-30 20:51:39 +0300

    MongoDB addition

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Get URI from environment variables
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # Create the Flask application instance
 app = Flask(__name__)
